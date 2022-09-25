cb49f7215ffafcd41b5e86c9e63f98d4678441a9
PhucDuong-SSS <phucdndev@gmail.com>
2022-09-25 14:37:38 +0700

    add search component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import SearchInput from '../components/SearchInput'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+        <SearchInput />
+    </div>
   )
 }
 
