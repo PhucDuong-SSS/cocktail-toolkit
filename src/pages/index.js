5350404e2b01cdeb8e87e5ad3d308e8053994e8b
PhucDuong-SSS <phucdndev@gmail.com>
2022-09-25 16:20:37 +0700

    render cocktail list

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import CocktailList from '../components/CocktailList'
 import SearchInput from '../components/SearchInput'
 
 function Home() {
   return (
     <div>
         <SearchInput />
+        <CocktailList/>
     </div>
   )
 }
